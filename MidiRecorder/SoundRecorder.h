fd807d212fdb7503b2ac1fa08568d3daa70a5571
Thierry Sansaricq <thx.dev@gmail.com>
2015-09-10 01:23:18 -0400

    Initial Commit

---
+++ MidiRecorder/SoundRecorder.h
@@ -0,0 +1,17 @@
+//
+//  SoundRecorder.h
+//  MidiRecorder
+//
+//  Created by Thierry on 9/10/15.
+//  Copyright (c) 2015 Thierry Sansaricq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include <AudioToolbox/AudioToolbox.h>
+//#include <CoreAudio/CoreAudioTypes.h>
+
+
+
+@interface SoundRecorder : NSObject
+
+@end
